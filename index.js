0e49d6b74d0334a1da440401ee234a74658e7ba7
nniemi <70947133+nniemi@users.noreply.github.com>
2021-09-22 22:05:23 +0300

    Change config

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config2 = require('./config2.json');
+const config = require('./config.json');
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 
@@ -6,11 +6,11 @@ let lines = [];
 
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(config2.TOKEN, {polling: true});
+const bot = new TelegramBot(config.TOKEN, {polling: true});
 
 // Creates a temporary array which includes all the files from the 
 // directory. The array changes while running the bot.
-let temp_files = fs.readdirSync(config2.DIRECTORY);
+let temp_files = fs.readdirSync(config.DIRECTORY);
 
 
 // Interacts with the user when the user gives /start command.
@@ -36,7 +36,7 @@ bot.onText(/\/quote/i, (msg) => {
         return;
     }
 
-    let filename = config2.QUOTES
+    let filename = config.QUOTES
     bot.sendMessage(msg.chat.id,get_line(filename))
 
 })
@@ -71,7 +71,7 @@ bot.onText(/\/addq/i,(msg) => {
         return;
     }
 
-    let filename = config2.QUOTES
+    let filename = config.QUOTES
     let quote = msg.text.substring(6)
     fs.appendFileSync(filename, '\n' + quote)
     bot.sendMessage(msg.chat.id, "quottista")
@@ -91,7 +91,7 @@ bot.onText(/\/äpö/i,(msg) => {
 
     let time = msg.text.substring(5)
 
-    let link = config2.FOOD_LINK
+    let link = config.FOOD_LINK
     
     bot.sendPoll(msg.chat.id, "Mihkä tänää äpölle " + time + "?",
                 [("Lé Reaktor"),("Newton"),("Hertsi"),("Café tietokonehuone")], 
@@ -116,7 +116,7 @@ bot.onText(/\/ruokalista/i,(msg) => {
 
     let time = msg.text.substring(5)
 
-    let link = config2.FOOD_LINK
+    let link = config.FOOD_LINK
     
     // Sends a link to a food menu.
     bot.sendMessage( msg.chat.id, "nauti " + link, 
@@ -159,7 +159,7 @@ bot.onText(/\/viis/i,(msg) => {
     }
 
     // Sends an empty gambina meeting template
-    let filename = config2.GAMBINA
+    let filename = config.GAMBINA
     let message = fs.readFileSync(filename)
     bot.sendMessage(msg.chat.id,message)
 
@@ -185,17 +185,17 @@ bot.onText(/\/kolikko/i,(msg) => {
    
    // If random integer is between 0 and 49, sends a photo of tails
    if(random_num >= 0 && random_num <= 49) {
-    let file = config2.COIN_DIRECTORY + "kruuna.png" 
+    let file = config.COIN_DIRECTORY + "kruuna.png" 
        bot.sendPhoto(msg.chat.id,fs.readFileSync(file), {caption: "kruuna boi"})
    } else if (random_num == 50) {
 
     // Then again if integer is exactly 50, a rare coin will be sent.
-    let file = config2.COIN_DIRECTORY  + "mayhem.png" 
+    let file = config.COIN_DIRECTORY  + "mayhem.png" 
     bot.sendPhoto(msg.chat.id,fs.readFileSync(file), {caption: "mayhemii isosti, kellota"})
    } else {
 
     // In other cases (between 51-100), a photo of heads will be posted.
-    let file = config2.COIN_DIRECTORY + "klaava.png" 
+    let file = config.COIN_DIRECTORY + "klaava.png" 
     bot.sendPhoto(msg.chat.id,fs.readFileSync(file), {caption: "klaava boi"})
    }
 
@@ -218,14 +218,14 @@ bot.onText(/\/kolikko/i,(msg) => {
     // If the array is empty, it will be refilled.
     if (file_amount == 0) {
         bot.sendMessage(msg.chat.id, "vidusti kaatumista lol")
-        temp_files = fs.readdirSync(config2.DIRECTORY);
+        temp_files = fs.readdirSync(config.DIRECTORY);
     }
 
     // Creates a random index.
     var random = Math.floor(Math.random() * file_amount)
 
     // Determines the path for the video that will be sent to the user.
-    let file = config2.DIRECTORY + temp_files[random]
+    let file = config.DIRECTORY + temp_files[random]
 
     // Deletes the corresponding video from the files array to avoid
     // duplicate videos in the current loop.
